3cc778a079fac262578786e0e40e5b593edea8b3
Brian Bidulock <bidulock@openss7.org>
2020-05-11 00:11:52 -0600

    properly initialize libcanberra

--- src/xde-lm-applet.c
+++ src/xde-lm-applet.c
@@ -68,6 +68,7 @@
 #include <sys/select.h>
 #include <sys/time.h>
 #include <sys/timerfd.h>
+#include <sys/eventfd.h>
 #include <sys/ioctl.h>
 #include <sys/wait.h>
 #include <sys/poll.h>
@@ -274,7 +275,18 @@ typedef enum {
 	CaEventSleepSuspend,
 	CaEventBatteryLevel,
 	CaEventThermalEvent,
+	CaEventBatteryState,
+	CaEventSystemChange,
+	CaEventMaximumContextId
 } CaEventId;
+
+struct EventQueue {
+	uint32_t context_id;
+	int efd;
+	GIOChannel *channel;
+	guint source_id;
+	GQueue *queue;
+} CaEventQueues[CaEventMaximumContextId-CA_CONTEXT_ID] = { {0, }, };
 
 typedef enum {
 	InputVoltmeterMissing = 0,
@@ -432,6 +444,68 @@ typedef struct {
 	char *adapter;
 	GList *features;
 } XdeChip;
+
+/** @section Queued Sound Functions
+  * @{ */
+
+void
+play_done(ca_context *ca, uint32_t id, int error_code, void *user_data)
+{
+	struct EventQueue *q = user_data;
+	uint64_t count = 1;
+
+	(void) ca;
+	(void) id;
+	(void) error_code;
+
+	if (!g_queue_is_empty(q->queue))
+		if (write(q->efd, &count, sizeof(count))) {}
+}
+
+void
+prop_free(gpointer data)
+{
+	ca_proplist *pl = data;
+
+	ca_proplist_destroy(pl);
+}
+
+int
+ca_context_cancel_queue(ca_context *ca, uint32_t id)
+{
+	if (id >= CA_CONTEXT_ID && id < CaEventMaximumContextId) {
+		struct EventQueue *q = &CaEventQueues[id - CA_CONTEXT_ID];
+
+		g_queue_clear_full(q->queue, prop_free);
+	}
+	return ca_context_cancel(ca, id);
+}
+
+int
+ca_context_play_queue(ca_context *ca, uint32_t id, ca_proplist *pl)
+{
+	int r;
+
+	if (id >= CA_CONTEXT_ID && id < CaEventMaximumContextId) {
+		struct EventQueue *q = &CaEventQueues[id - CA_CONTEXT_ID];
+		int playing = 0;
+
+		ca_context_playing(ca, id, &playing);
+		if (playing || !g_queue_is_empty(q->queue)) {
+			g_queue_push_tail(q->queue, pl);
+			return CA_SUCCESS;
+		}
+		r = ca_context_play_full(ca, id, pl, play_done, q);
+		ca_proplist_destroy(pl);
+		return (r);
+	}
+	r = ca_context_play_full(ca, id, pl, NULL, NULL);
+	ca_proplist_destroy(pl);
+	return (r);
+}
+
+
+/** @} */
 
 void get_status_window(XdeScreen *xscr);
 
@@ -1527,17 +1601,76 @@ init_sensors(XdeScreen *xscr)
 	}
 }
 
+gboolean
+queue_call(GIOChannel *channel, GIOCondition condition, gpointer data)
+{
+	struct EventQueue *q = data;
+	uint64_t count = 0;
+
+	(void) channel;
+	(void) condition;
+
+	if (read(q->efd, &count, sizeof(count)) >= 0) {
+		ca_context *ca = get_default_ca_context();
+		int playing = 0;
+
+		ca_context_playing(ca, q->context_id, &playing);
+		if (!playing) {
+			ca_proplist *pl;
+
+			while ((pl = g_queue_pop_head(q->queue))) {
+				int r;
+
+				r = ca_context_play_full(ca, q->context_id, pl, play_done, q);
+				ca_proplist_destroy(pl);
+				if (r == CA_SUCCESS)
+					break;
+			}
+		}
+	}
+	return G_SOURCE_CONTINUE;
+}
+
 void
 init_canberra(void)
 {
 	ca_context *ca = get_default_ca_context();
 	ca_proplist *pl;
+	int theme_set = 0;
+	int i;
 
 	ca_proplist_create(&pl);
 	ca_proplist_sets(pl, CA_PROP_APPLICATION_ID, "com.unexicon." RESNAME);
 	ca_proplist_sets(pl, CA_PROP_APPLICATION_VERSION, VERSION);
 	ca_proplist_sets(pl, CA_PROP_APPLICATION_ICON_NAME, LOGO_NAME);
 	ca_proplist_sets(pl, CA_PROP_APPLICATION_LANGUAGE, "C");
+	ca_proplist_sets(pl, CA_PROP_CANBERRA_VOLUME, "0.0");
+	if (!theme_set) {
+		GdkDisplay *disp = gdk_display_get_default();
+		GdkScreen *scrn = gdk_display_get_default_screen(disp);
+		GtkSettings *set = gtk_settings_get_for_screen(scrn);
+		GValue theme_v = G_VALUE_INIT;
+		const char *stheme;
+
+		gtk_rc_reparse_all();
+
+		g_value_init(&theme_v, G_TYPE_STRING);
+		g_object_get_property(G_OBJECT(set), "gtk-sound-theme-name", &theme_v);
+		stheme = g_value_get_string(&theme_v);
+		if (stheme) {
+			DPRINTF(1, "Setting sound theme to %s\n", stheme);
+			ca_proplist_sets(pl, CA_PROP_CANBERRA_XDG_THEME_NAME, stheme);
+			theme_set = 1;
+		}
+		g_value_unset(&theme_v);
+	}
+	if (!theme_set) {
+		EPRINTF("Could not set theme!\n");
+		ca_proplist_sets(pl, CA_PROP_CANBERRA_XDG_THEME_NAME, "freedesktop");
+		theme_set = 1;
+	}
+	ca_proplist_sets(pl, CA_PROP_CANBERRA_XDG_THEME_OUTPUT_PROFILE, "stereo");
+	ca_proplist_sets(pl, CA_PROP_CANBERRA_ENABLE, "1");
 	{
 		char pidstring[64];
 
@@ -1552,6 +1685,17 @@ init_canberra(void)
 		ca_proplist_sets(pl, CA_PROP_APPLICATION_PROCESS_HOST, hostname);
 	}
 	ca_context_change_props_full(ca, pl);
+	for (i = 0; i < CaEventMaximumContextId - CA_CONTEXT_ID; i++) {
+		struct EventQueue *q = &CaEventQueues[i];
+
+		q->context_id = i + CA_CONTEXT_ID;
+		if ((q->efd = eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK | EFD_SEMAPHORE)) >= 0) {
+			if ((q->channel = g_io_channel_unix_new(q->efd))) {
+				q->queue = g_queue_new();
+				q->source_id = g_io_add_watch(q->channel, G_IO_IN, queue_call, q);
+			}
+		}
+	}
 }
 
 void
